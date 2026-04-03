ff36d4aa67398d9e84ad989ff581bc057f6c1ac6
lightschamp <lightschamp@gmail.com>
2026-04-03 15:49:04 +0100

    Add files via upload

---
+++ dinosaurvscoderun.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo myServo;
+int ldrPin = A0;
+int servoPin = 8;
+int light;
+int pos;
+
+void setup(){
+    Serial.begin(9600);
+    pinMode(ldrPin, INPUT);
+    myServo.attach(servoPin);
+
+
+}
+
+void loop(){
+    delay(10);
+    light = analogRead(ldrPin);
+    Serial.println(light);
+    if (light < 810){
+        myServo.write(0);
+        delay(60);
+        myServo.write(180);
+        delay(60);
+        myServo.write(90);
+        delay(150);
+
+    }
+    else{
+        myServo.write(90);
+
+    }
+}
+
